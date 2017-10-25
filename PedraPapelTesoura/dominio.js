038cf2a48632e7ba09d24151ffb5cd6e6bf49e2a
Tyrone Damasceno <tyronedamasceno@gmail.com>
2017-10-25 17:26:08 -0300

    Starting work

--- PedraPapelTesoura/dominio.js
+++ PedraPapelTesoura/dominio.js
@@ -1,5 +1,47 @@
+var listaJogadores = [];
+var listaDuplas = [];
+
+function Dupla(jogador1, jogador2) {
+	if (jogador1.nome > jogador2.nome) {
+		aux = jogador1;
+		jogador1 = jogador2;
+		jogador2 = aux;
+	}
+	this.jogador1 = jogador1;
+	this.jogador2 = jogador2;
+	this.pontosJ1 = 0;
+	this.pontosJ2 = 0;
+	this.jogadasJ1 = [];
+	this.jogadasJ2 = [];
+}
+
 function Jogador(nome, sexo, tipoJogador) {
 	this.nome = nome;
-	this.sexo = sexo;
-	this.tipoJogador = tipoJogador;
+	if (sexo[0].toUpperCase() == 'M') this.sexo = 'Masculino';
+	else this.sexo = 'Feminino';
+	if (tipoJogador[0].toUpperCase()	 == 'H') this.tipoJogador = 'Humano';
+	else this.tipoJogador = 'Computador';
+}
+
+function findJogador(jogador) {
+	for(var i = 0; i < listaJogadores.length; i++) {
+		if (listaJogadores[i].nome == jogador.nome)
+			return i;
+	}
+	return -1;
+}
+
+function findDupla(dupla) {
+	for (var i = 0; i < listaDuplas.length; i++) {
+		if (listaDuplas[i].jogador1.nome == dupla.jogador1.nome
+			&& listaDuplas[i].jogador2.nome == dupla.jogador2.nome)
+			return i;
+	}
+	return -1;
+}
+
+function addJogador(jogador) {
+	if (findJogador(jogador) == -1) {
+		listaJogadores.push(jogador);
+	}
 }

4ea29c7b97b47415ee67b550b9c361eb9f85655e
Tyrone Damasceno <tyronedamasceno@gmail.com>
2017-10-27 13:30:56 -0300

    Melhorando manipulação do DOM e organizando código

--- PedraPapelTesoura/domManipulation.js
+++ PedraPapelTesoura/domManipulation.js
@@ -1,28 +1,41 @@
 function inicializaTabela() {
-	var nome1 = document.getElementById("placarJ1");
-	var nome2 = document.getElementById("placarJ2");
-	nome1.innerHTML = listaDuplas[duplaAtualIndex].jogador1.nome;
-	nome2.innerHTML = listaDuplas[duplaAtualIndex].jogador2.nome;
+
+	var nome1 = document.getElementById("placarJ1").innerHTML = listaDuplas[duplaAtualIndex].jogador1.nome;
+	var nome2 = document.getElementById("placarJ2").innerHTML = listaDuplas[duplaAtualIndex].jogador2.nome;
+	
 	var tabela = document.getElementById("Placar");
+	
 	while (tabela.rows.length >= 2) {
 		tabela.deleteRow(1);
 	}
+
 	var linha = tabela.insertRow();
 	linha.insertCell().innerHTML = listaDuplas[duplaAtualIndex].pontosJ1;
 	linha.insertCell().innerHTML = listaDuplas[duplaAtualIndex].pontosJ2;
+	
 	for (var i = 0; i < listaDuplas[duplaAtualIndex].jogadasJ1.length; i++) {
+		var imgJ1 = listaDuplas[duplaAtualIndex].jogadasJ1[i].toLowerCase() + '.jpg';
+		var imgJ2 = listaDuplas[duplaAtualIndex].jogadasJ2[i].toLowerCase() + '.jpg';
+
 		var linha = tabela.insertRow();
-		linha.insertCell().innerHTML = listaDuplas[duplaAtualIndex].jogadasJ1[i];
-		linha.insertCell().innerHTML = listaDuplas[duplaAtualIndex].jogadasJ2[i];
+		linha.insertCell().innerHTML = "<img src="+imgJ1+" class='naTabela'/>";	
+		linha.insertCell().innerHTML = "<img src="+imgJ2+" class='naTabela'/>";
 	}
 }
 
 function atualizaTabela() {
+	
 	var tabela = document.getElementById("Placar");
+
 	tabela.rows[1].cells[0].innerHTML = listaDuplas[duplaAtualIndex].pontosJ1;
 	tabela.rows[1].cells[1].innerHTML = listaDuplas[duplaAtualIndex].pontosJ2;
+	
 	var linha = tabela.insertRow();
 	var pos = listaDuplas[duplaAtualIndex].jogadasJ1.length-1;
-	linha.insertCell().innerHTML = listaDuplas[duplaAtualIndex].jogadasJ1[pos];
-	linha.insertCell().innerHTML = listaDuplas[duplaAtualIndex].jogadasJ2[pos];
+
+	var imgJ1 = listaDuplas[duplaAtualIndex].jogadasJ1[pos].toLowerCase() + '.jpg';
+	var imgJ2 = listaDuplas[duplaAtualIndex].jogadasJ2[pos].toLowerCase() + '.jpg';
+
+	linha.insertCell().innerHTML = "<img src="+imgJ1+" class='naTabela'/>";
+	linha.insertCell().innerHTML = "<img src="+imgJ2+" class='naTabela'/>";
 }
